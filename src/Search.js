0504434f060e953a910068d34fa5f35b42efc5c6
Caitlin McCarthy <cates.mccarthy@gmail.com>
2020-02-16 19:29:14 -0600

    added checkboxes

--- src/Search.js
+++ src/Search.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import Modal from 'react-responsive-modal';
 import './App.css';
-import SearchInput, {createFilter} from 'react-search-input';
+import SearchInput, { createFilter } from 'react-search-input';
 
 const KEYS_TO_FILTERS = ['first', 'last', 'team'];
 
@@ -43,7 +43,7 @@ class Search extends Component {
           console.log('error', error)
         } // errorCallback
       );
-      this.setState({searchTerm: ''});
+      this.setState({ searchTerm: '' });
     }
   }
 
@@ -63,7 +63,7 @@ class Search extends Component {
     } else if (!!dancer.xxxl) {
       size = '3XL';
     }
-    return({size, color})
+    return ({ size, color })
   }
 
   renderDancer = (dancer, props) => {
@@ -88,11 +88,11 @@ class Search extends Component {
 
   render() {
     let data = Object.values(this.props.data[0])[3];
-    const filteredDancers = data.filter(createFilter(this.state.searchTerm),  KEYS_TO_FILTERS);
+    const filteredDancers = data.filter(createFilter(this.state.searchTerm), KEYS_TO_FILTERS);
 
     return (
       <div>
-        <SearchInput className="search-input" onChange={this.searchUpdated} fuzzy style={styles.search}/>
+        <SearchInput className="search-input" onChange={this.searchUpdated} fuzzy style={styles.search} />
         <div style={styles.dancers}>
           {filteredDancers.map((dancer, id) => {
             if (!dancer.present) {
@@ -106,10 +106,37 @@ class Search extends Component {
         <div>
           <Modal open={this.state.open} onClose={this.onCloseModal} center>
             <div style={styles.modalContent}>
-              <h2>Are you sure you want to check in?</h2>
-              <h3>{!!this.state.dancer ? this.state.dancer.first + ' ' + this.state.dancer.last  + ', ' + this.state.dancer.team : null}</h3>
-              <p>{!!this.state.dancer && !!this.state.dancer.shirt ? this.state.dancer.shirt.size + ', ' + this.state.dancer.shirt.color: null}</p>
-              <button onClick={() => this.checkIn(this.state.dancer, this.props, parseInt(this.state.dancer.id) + 1)}>CHECK IN!</button>
+              <div>
+                <form onSubmit="return checkForm(this)">
+                  <h2>Please accept these terms to proceed</h2>
+                  <p>Releasor realizes that participation in the HuskerThon 2020 involves certain risks and danger and is a vigorous activity involving severe respiratory and cardiovascular stress. Releasor has hereby been made aware that participation in HuskerThon 2020 has the following non-exclusive list of certain risks which I accept: death; head, eye, neck, and spinal injury resulting in complete or partial paralysis; brain damage; heart attack; blisters; cuts; lacerations; abrasions; concussions; contusions; strains; sprains; dislocations; fractures; cold and heat injuries; water immersion; drowning; lightning strikes; injury to bones, joints, muscles, internal organs; and environmental conditions.</p>
+                  <p>In consideration of participation in HuskerThon 2020 Releasor hereby RELEASES and covenants not-to-sue the UNIVERSITY for any and all present and future claims resulting from ordinary negligence on the part of the UNIVERSITY for property damage, personal injury, or wrongful death arising as a result of my engaging in, using University facilities and equipment, or receiving instruction for HuskerThon 2020 or activities thereto, wherever, whenever, or however the same may occur. Releasor hereby voluntarily waives any and all claims or actions resulting from ordinary negligence, both present and future, that may be made by Releasor’s family, estate, personal representative, heirs, or assigns.</p>
+                  <input type="checkbox" name="waiver" required></input>
+                  <label for="waiver">Agree</label>
+                  <br></br>
+                  <p><strong>Have you ever been convicted of any of the following crimes</strong></p>
+                  <ul>
+                    <li>Felony assault, including domestic violence related incidents</li>
+                    <li>Child abuse, molestation or other crime involving endangerment of a minor</li>
+                    <li>Murder</li>
+                    <li>Kidnapping</li>
+                    <li>Misdemeanor assault</li>
+                    <li>Drug distribution activity</li>
+                    <li>Felony drug possession, and any other felony</li>
+                    <li>Crime involving moral turpitude</li>
+                  </ul>
+                  <input type="radio" id="crimeYes" name="crime" value="1" required></input>
+                  <label for="crimeYes">Yes, I <strong>have</strong> committed one of the above crimes</label>
+                  <br></br>
+                  <input type="radio" name="crime" value="0" required></input>
+                  <label for="crimeNo">No, I <strong>have not</strong> committed one of the above crimes</label>
+                  <p><input type="submit">Submit waiver</input></p>
+                </form>
+                <h2>Are you sure you want to check in?</h2>
+                <h3>{!!this.state.dancer ? this.state.dancer.first + ' ' + this.state.dancer.last + ', ' + this.state.dancer.team : null}</h3>
+                <p>{!!this.state.dancer && !!this.state.dancer.shirt ? this.state.dancer.shirt.size + ', ' + this.state.dancer.shirt.color : null}</p>
+                <button onClick={() => this.checkIn(this.state.dancer, this.props, parseInt(this.state.dancer.id) + 1)}>CHECK IN!</button>
+              </div>
             </div>
           </Modal>
         </div>
@@ -117,8 +144,12 @@ class Search extends Component {
     );
   }
 
-  searchUpdated (term) {
-    this.setState({searchTerm: term})
+  checkForm(form) {
+    
+  }
+
+  searchUpdated(term) {
+    this.setState({ searchTerm: term })
   }
 }
 
