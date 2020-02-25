1329a17ea9893e550abd50c0bf596f4571a5dfaa
Caitlin McCarthy <cates.mccarthy@gmail.com>
2020-02-25 13:15:10 -0600

    waiver modal working - not pretty

--- src/Search.js
+++ src/Search.js
@@ -12,10 +12,37 @@ class Search extends Component {
     this.state = {
       searchTerm: '',
       open: false,
+      waiver: false,
+      waiverOpen: false,
       dancer: null,
     }
     this.searchUpdated = this.searchUpdated.bind(this)
   }
+
+  getState() {
+    return this.state;
+  }
+
+  onOpenWaiver = (dancer) => {
+    this.setState({
+      waiverOpen: true,
+      dancer,
+    });
+  };
+
+  onCloseWaiver = (dancer) => {
+    console.log("onClose has been hit");
+    this.setState({
+      waiverOpen: false,
+      dancer,
+    });
+    if(this.state.waiver) {
+      this.setState({
+        open: true,
+      });
+    }
+  }
+
 
   onOpenModal = (dancer) => {
     this.setState({
@@ -38,6 +65,16 @@ class Search extends Component {
         'L', // column
         row, // row
         'X', // value
+        props.callback, // successCallback
+        (error) => {
+          console.log('error', error)
+        } // errorCallback
+      );
+      props.props.updateCell(
+        'Sheet1', // sheetName
+        'M', // column
+        row, // row
+        dancer.NUID, // value
         props.callback, // successCallback
         (error) => {
           console.log('error', error)
@@ -81,7 +118,7 @@ class Search extends Component {
             <div style={styles.shirtInfo} key={dancer.shirt.color}><strong>{dancer.shirt.color}</strong></div>
           </div>
         </div>
-        <div style={styles.button}><button onClick={() => this.onOpenModal(dancer)}>CHECK IN</button></div>
+        <div style={styles.button}><button onClick={() => this.onOpenWaiver(dancer)}>CHECK IN</button></div>
       </div>
     );
   }
@@ -104,48 +141,64 @@ class Search extends Component {
           })}
         </div>
         <div>
-          <Modal open={this.state.open} onClose={this.onCloseModal} center>
+          <Modal open={this.state.waiverOpen} onClose={this.onCloseWaiver} center>
             <div style={styles.modalContent}>
-              <div>
-                <form onSubmit="return checkForm(this)">
-                  <h2>Please accept these terms to proceed</h2>
-                  <p>Releasor realizes that participation in the HuskerThon 2020 involves certain risks and danger and is a vigorous activity involving severe respiratory and cardiovascular stress. Releasor has hereby been made aware that participation in HuskerThon 2020 has the following non-exclusive list of certain risks which I accept: death; head, eye, neck, and spinal injury resulting in complete or partial paralysis; brain damage; heart attack; blisters; cuts; lacerations; abrasions; concussions; contusions; strains; sprains; dislocations; fractures; cold and heat injuries; water immersion; drowning; lightning strikes; injury to bones, joints, muscles, internal organs; and environmental conditions.</p>
-                  <p>In consideration of participation in HuskerThon 2020 Releasor hereby RELEASES and covenants not-to-sue the UNIVERSITY for any and all present and future claims resulting from ordinary negligence on the part of the UNIVERSITY for property damage, personal injury, or wrongful death arising as a result of my engaging in, using University facilities and equipment, or receiving instruction for HuskerThon 2020 or activities thereto, wherever, whenever, or however the same may occur. Releasor hereby voluntarily waives any and all claims or actions resulting from ordinary negligence, both present and future, that may be made by Releasor’s family, estate, personal representative, heirs, or assigns.</p>
-                  <input type="checkbox" name="waiver" required></input>
-                  <label for="waiver">Agree</label>
-                  <br></br>
-                  <p><strong>Have you ever been convicted of any of the following crimes</strong></p>
-                  <ul>
-                    <li>Felony assault, including domestic violence related incidents</li>
-                    <li>Child abuse, molestation or other crime involving endangerment of a minor</li>
-                    <li>Murder</li>
-                    <li>Kidnapping</li>
-                    <li>Misdemeanor assault</li>
-                    <li>Drug distribution activity</li>
-                    <li>Felony drug possession, and any other felony</li>
-                    <li>Crime involving moral turpitude</li>
-                  </ul>
-                  <input type="radio" id="crimeYes" name="crime" value="1" required></input>
-                  <label for="crimeYes">Yes, I <strong>have</strong> committed one of the above crimes</label>
-                  <br></br>
-                  <input type="radio" name="crime" value="0" required></input>
-                  <label for="crimeNo">No, I <strong>have not</strong> committed one of the above crimes</label>
-                  <p><input type="submit">Submit waiver</input></p>
-                </form>
-                <h2>Are you sure you want to check in?</h2>
-                <h3>{!!this.state.dancer ? this.state.dancer.first + ' ' + this.state.dancer.last + ', ' + this.state.dancer.team : null}</h3>
-                <p>{!!this.state.dancer && !!this.state.dancer.shirt ? this.state.dancer.shirt.size + ', ' + this.state.dancer.shirt.color : null}</p>
-                <button onClick={() => this.checkIn(this.state.dancer, this.props, parseInt(this.state.dancer.id) + 1)}>CHECK IN!</button>
-              </div>
+              <form onSubmit={this.checkForm}>
+                <h2>Please accept these terms to proceed</h2>
+                <p>Releasor realizes that participation in the HuskerThon 2020 involves certain risks and danger and is a vigorous activity involving severe respiratory and cardiovascular stress. Releasor has hereby been made aware that participation in HuskerThon 2020 has the following non-exclusive list of certain risks which I accept: death; head, eye, neck, and spinal injury resulting in complete or partial paralysis; brain damage; heart attack; blisters; cuts; lacerations; abrasions; concussions; contusions; strains; sprains; dislocations; fractures; cold and heat injuries; water immersion; drowning; lightning strikes; injury to bones, joints, muscles, internal organs; and environmental conditions.</p>
+                <p>In consideration of participation in HuskerThon 2020 Releasor hereby RELEASES and covenants not-to-sue the UNIVERSITY for any and all present and future claims resulting from ordinary negligence on the part of the UNIVERSITY for property damage, personal injury, or wrongful death arising as a result of my engaging in, using University facilities and equipment, or receiving instruction for HuskerThon 2020 or activities thereto, wherever, whenever, or however the same may occur. Releasor hereby voluntarily waives any and all claims or actions resulting from ordinary negligence, both present and future, that may be made by Releasor’s family, estate, personal representative, heirs, or assigns.</p>
+                <input type="checkbox" name="waiver" required></input>
+                <label>Agree</label>
+                <br></br>
+                <h2><strong>Have you ever been convicted of any of the following crimes</strong></h2>
+                <ul>
+                  <li>Felony assault, including domestic violence related incidents</li>
+                  <li>Child abuse, molestation or other crime involving endangerment of a minor</li>
+                  <li>Murder</li>
+                  <li>Kidnapping</li>
+                  <li>Misdemeanor assault</li>
+                  <li>Drug distribution activity</li>
+                  <li>Felony drug possession, and any other felony</li>
+                  <li>Crime involving moral turpitude</li>
+                </ul>
+
+                <input type="radio" name="crime" value="1" required></input>
+                <label><strong>YES</strong>, I <strong>have</strong> committed one of the above crimes</label>
+                <br></br>
+                <input type="radio" name="crime" value="0" required></input>
+                <label><strong>NO</strong>, I <strong>have not</strong> committed one of the above crimes</label>
+                <br></br><br></br>
+                <label>Please enter your NUID: </label>
+                <input type="text" id="NUID" required></input>
+                <br></br><br></br>
+                <input type="submit"></input>
+              </form>
             </div>
+          </Modal>
+          <Modal open={this.state.open} onClose={this.onCloseModal} center>
+            <h2>Are you sure you want to check in?</h2>
+            <h3>{!!this.state.dancer ? this.state.dancer.first + ' ' + this.state.dancer.last + ', ' + this.state.dancer.team : null}</h3>
+            <p>{!!this.state.dancer && !!this.state.dancer.shirt ? this.state.dancer.shirt.size + ', ' + this.state.dancer.shirt.color : null}</p>
+            <button onClick={() => this.checkIn(this.state.dancer, this.props, parseInt(this.state.dancer.id) + 1)}>CHECK IN!</button>
           </Modal>
         </div>
       </div>
     );
   }
 
-  checkForm(form) {
-    
+  checkForm = (form) => {
+    form.preventDefault();
+    const noCrime = document.getElementsByName("crime")[1].checked;
+    const id = document.getElementById("NUID").value;
+    const dancer = this.state.dancer;
+    if(noCrime) {
+      this.setState({
+        open: true,
+      });
+      dancer.NUID = id;
+    } else {
+      alert("Please talk to a staff member... You have asserted that you have committed a crime.");
+    }
   }
 
   searchUpdated(term) {
