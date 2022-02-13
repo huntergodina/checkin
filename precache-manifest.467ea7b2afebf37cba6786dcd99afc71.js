8cae8ea3a370422f63df1623dd9679b79c4f2182
Hunter Godina <huntergodina@gmail.com>
2022-02-13 12:48:22 -0600

    Updates

---
+++ precache-manifest.467ea7b2afebf37cba6786dcd99afc71.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b941ac19e22158e0d19",
+    "url": "/checkin/static/css/main.4346e92e.chunk.css"
+  },
+  {
+    "revision": "0b941ac19e22158e0d19",
+    "url": "/checkin/static/js/main.0b941ac1.chunk.js"
+  },
+  {
+    "revision": "dadc3ef0b9b07b1f9bb5",
+    "url": "/checkin/static/js/1.dadc3ef0.chunk.js"
+  },
+  {
+    "revision": "ea08932aa41683af12f9",
+    "url": "/checkin/static/js/runtime~main.ea08932a.js"
+  },
+  {
+    "revision": "838be0eba07fcb5aeeb9dd129a6d085c",
+    "url": "/checkin/index.html"
+  }
+];
